5079d44e752c3b85089543b31f2d902144d6b2e9
Ivan Kovalchuk <ISKovalchuk.SBT@sberbank.ru>
2019-10-24 16:21:08 +0700

    ServerConnector modification

--- src/main/java/org/bees/optimizer/service/ServerConnector.java
+++ src/main/java/org/bees/optimizer/service/ServerConnector.java
@@ -1,5 +1,54 @@
 package org.bees.optimizer.service;
 
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.bees.optimizer.model.external.LoginDto;
+import org.bees.optimizer.model.external.ReconnectDto;
+import org.bees.optimizer.model.external.RouteDto;
+import org.bees.optimizer.model.external.TokenDto;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class ServerConnector {
 
+    @NonNull
+    private RestTemplate restTemplate;
+
+    public void sendTeam(LoginDto team) {
+        try {
+//            ResponseEntity<Response> rs = restTemplate.postForEntity(exportUrl(), campaignDTOBy(campaign),
+//                    Response.class);
+//            processResponse(rs);
+        } catch (HttpClientErrorException ex) {
+            log.error("Team name sending error: " + ex.getResponseBodyAsString(), ex);
+        } catch (Exception ex) {
+            log.error("Team name sending error: ", ex);
+        }
+    }
+
+    public void sendCarsRoute (TokenDto tokenDto, RouteDto routeDto) {
+        try {
+
+        } catch (HttpClientErrorException ex) {
+            log.error("Cars route sending error: " + ex.getResponseBodyAsString(), ex);
+        } catch (Exception ex) {
+            log.error("Cars route sending error: ", ex);
+        }
+    }
+
+    public void reconnect (ReconnectDto reconnectDto) {
+        try {
+
+        } catch (HttpClientErrorException ex) {
+            log.error("Cars route sending error: " + ex.getResponseBodyAsString(), ex);
+        } catch (Exception ex) {
+            log.error("Cars route sending error: ", ex);
+        }
+
+    }
 }

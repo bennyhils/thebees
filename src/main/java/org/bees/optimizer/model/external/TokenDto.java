077768015f28885b0181a51c1249a519227660b2
Ivan Demidov <ISDemidoff@gmail.com>
2019-10-24 12:25:59 +0300

    Extended tests on dto format

--- src/main/java/org/bees/optimizer/model/external/TokenDto.java
+++ src/main/java/org/bees/optimizer/model/external/TokenDto.java
@@ -3,10 +3,12 @@ package org.bees.optimizer.model.external;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.util.List;
 
 @Getter
+@ToString
 public class TokenDto {
 
     private final String token;
--- src/test/java/org/bees/optimizer/model/external/DtoTest.java
+++ src/test/java/org/bees/optimizer/model/external/DtoTest.java
@@ -7,6 +7,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 public class DtoTest {
@@ -22,7 +24,10 @@ public class DtoTest {
         String json = "{ \"token\" : \"12321\", \"cars\": [\"sb1\", \"sb2\"], \"level\": 1 }";
 
         TokenDto token = mapper.readValue(json, TokenDto.class);
-        log.info("TokenDto values:\ntoken={}\ncars={}\nlevel={}", token.getToken(), token.getCars(), token.getLevel());
+        log.info("TokenDto:\n{}", token);
+        Assert.assertEquals("12321", token.getToken());
+        Assert.assertEquals(Arrays.asList("sb1", "sb2"), token.getCars());
+        Assert.assertEquals(1, token.getLevel());
     }
 
     @Test
@@ -35,6 +40,25 @@ public class DtoTest {
 
         RoutesDto routes = mapper.readValue(json, RoutesDto.class);
         log.info("RoutesDto:\n{}", routes);
+
+        List<RouteDto> routesList = routes.getRoutes();
+        Assert.assertEquals(3, routesList.size());
+
+        RouteDto routeDto0 = routesList.get(0);
+        Assert.assertEquals(1, routeDto0.getFrom());
+        Assert.assertEquals(7, routeDto0.getTo());
+        Assert.assertEquals(31, routeDto0.getTime());
+
+        RouteDto routeDto1 = routesList.get(1);
+        Assert.assertEquals(6, routeDto1.getFrom());
+        Assert.assertEquals(30, routeDto1.getTo());
+        Assert.assertEquals(1, routeDto1.getTime());
+
+        RouteDto routeDto2 = routesList.get(2);
+        Assert.assertEquals(10, routeDto2.getFrom());
+        Assert.assertEquals(17, routeDto2.getTo());
+        Assert.assertEquals(12, routeDto2.getTime());
+
     }
 
     @Test
@@ -47,6 +71,23 @@ public class DtoTest {
 
         TrafficDto traffic = mapper.readValue(json, TrafficDto.class);
         log.info("TrafficDto:\n{}", traffic);
+
+        List<TrafficJamDto> trafficList = traffic.getTraffic();
+
+        TrafficJamDto trafficJamDto0 = trafficList.get(0);
+        Assert.assertEquals(1, trafficJamDto0.getFrom());
+        Assert.assertEquals(7, trafficJamDto0.getTo());
+        Assert.assertEquals(1.0, trafficJamDto0.getJam(), 1e-15);
+
+        TrafficJamDto trafficJamDto1 = trafficList.get(1);
+        Assert.assertEquals(6, trafficJamDto1.getFrom());
+        Assert.assertEquals(30, trafficJamDto1.getTo());
+        Assert.assertEquals(1.5, trafficJamDto1.getJam(), 1e-15);
+
+        TrafficJamDto trafficJamDto2 = trafficList.get(2);
+        Assert.assertEquals(10, trafficJamDto2.getFrom());
+        Assert.assertEquals(17, trafficJamDto2.getTo());
+        Assert.assertEquals(1.9, trafficJamDto2.getJam(), 1e-15);
     }
 
     @Test
@@ -59,6 +100,24 @@ public class DtoTest {
 
         TrafficDto traffic = mapper.readValue(json, TrafficDto.class);
         log.info("TrafficDto:\n{}", traffic);
+
+        List<TrafficJamDto> trafficList = traffic.getTraffic();
+        Assert.assertEquals(3, trafficList.size());
+
+        TrafficJamDto trafficJamDto0 = trafficList.get(0);
+        Assert.assertEquals(1, trafficJamDto0.getFrom());
+        Assert.assertEquals(7, trafficJamDto0.getTo());
+        Assert.assertEquals(1.0, trafficJamDto0.getJam(), 1e-15);
+
+        TrafficJamDto trafficJamDto1 = trafficList.get(1);
+        Assert.assertEquals(6, trafficJamDto1.getFrom());
+        Assert.assertEquals(30, trafficJamDto1.getTo());
+        Assert.assertEquals(1.5, trafficJamDto1.getJam(), 1e-15);
+
+        TrafficJamDto trafficJamDto2 = trafficList.get(2);
+        Assert.assertEquals(10, trafficJamDto2.getFrom());
+        Assert.assertEquals(17, trafficJamDto2.getTo());
+        Assert.assertEquals(1.9, trafficJamDto2.getJam(), 1e-15);
     }
 
     @Test
@@ -72,6 +131,25 @@ public class DtoTest {
 
         PointsDto points = mapper.readValue(json, PointsDto.class);
         log.info("PointsDto:\n{}", points);
+
+        List<PointDto> pointsList = points.getPoints();
+        Assert.assertEquals(4, pointsList.size());
+
+        PointDto pointDto0 = pointsList.get(0);
+        Assert.assertEquals(0, pointDto0.getIndex());
+        Assert.assertEquals(1000200, pointDto0.getMoney());
+
+        PointDto pointDto1 = pointsList.get(1);
+        Assert.assertEquals(1, pointDto1.getIndex());
+        Assert.assertEquals(1000234, pointDto1.getMoney());
+
+        PointDto pointDto2 = pointsList.get(2);
+        Assert.assertEquals(2, pointDto2.getIndex());
+        Assert.assertEquals(1323200, pointDto2.getMoney());
+
+        PointDto pointDto3 = pointsList.get(3);
+        Assert.assertEquals(3, pointDto3.getIndex());
+        Assert.assertEquals(1434545, pointDto3.getMoney());
     }
 
     @Test
@@ -80,6 +158,9 @@ public class DtoTest {
 
         ArriveDto arriveDto = mapper.readValue(json, ArriveDto.class);
         log.info("ArriveDto:\n{}", arriveDto);
+
+        Assert.assertEquals(2, arriveDto.getPoint());
+        Assert.assertEquals("sb4", arriveDto.getCarName());
     }
 
     //---------------

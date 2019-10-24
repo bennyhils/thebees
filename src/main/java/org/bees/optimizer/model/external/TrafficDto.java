b153b7f2c29edbbc63bf8c62cf004d512806e9b6
Ivan Demidov <ISDemidoff@gmail.com>
2019-10-25 02:29:04 +0300

    extended trafficdto

--- src/main/java/org/bees/optimizer/model/external/TrafficDto.java
+++ src/main/java/org/bees/optimizer/model/external/TrafficDto.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Getter
 public class TrafficDto {
@@ -20,5 +21,15 @@ public class TrafficDto {
     @Override
     public String toString() {
         return traffic.stream().map(TrafficJamDto::toString).collect(Collectors.joining("\n"));
+    }
+
+    public TrafficDto getExtendedTraffic() {
+        return new TrafficDto(traffic
+                .stream()
+                .flatMap(t -> Stream.of(
+                        new TrafficJamDto(t.getFrom(), t.getTo(), t.getJam()),
+                        new TrafficJamDto(t.getTo(), t.getFrom(), t.getJam())
+                ))
+                .collect(Collectors.toList()));
     }
 }

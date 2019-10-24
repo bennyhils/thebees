654fe60c5b8fc989c2399a103163fc2e66581d99
Евгений Кузьмин <kuzmin.e.aleks@sberbank.ru>
2019-10-24 10:36:21 +0300

    master spring-boot first

---
+++ src/main/java/org/bees/optimizer/RouteOptimizerConfig.java
@@ -0,0 +1,11 @@
+package org.bees.optimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteOptimizerConfig {
+    public static void main(String[] args) {
+        SpringApplication.run(RouteOptimizerConfig.class, args);
+    }
+}
